fc448f59c8ef4c0fdad749379a34bc42dee8c2ae
John Evdemon <C:\Users\jevde\AppData\Roaming\The Bat!>
2026-09-11 01:53:14 -0700

    Merge PR#1 notification fix, rename app to Flip2Calendar, fix app label, lower minSdk to 29

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
 
     defaultConfig {
         applicationId = "com.evdeman.flip2calendar"
-        minSdk = 30
+        minSdk = 29
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
